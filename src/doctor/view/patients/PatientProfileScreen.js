26597f96757c943ed907ee51cf4b1ff4b62c02c3
jrazi <j.razi@outlook.com>
2020-12-11 08:17:37 +0330

    Implement new visit dialog

--- src/doctor/view/patients/PatientProfileScreen.js
+++ src/doctor/view/patients/PatientProfileScreen.js
@@ -2,17 +2,14 @@
 import React from "react";
 import {StyleSheet, View} from "react-native";
 import {ScreenHeader, ScreenLayout} from "../../../root/view/screen/Layout";
-import {Button, Text} from "react-native-paper";
-import {rootDao} from "../../../root/data/dao/RootDao";
-import {doctorDao} from "../../data/dao/DoctorDao";
+import {doctorDao, VisitState} from "../../data/dao/DoctorDao";
 import {hasValue} from "../../../root/domain/util/Util";
 import {currentTheme} from "../../../../theme";
 import HomeScreen from "../HomeScreen";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import PatientsScreen from "./PatientsScreen";
 import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
-import {debugBorderRed} from "../../../root/view/styles/borders";
-import { FAB } from 'react-native-paper';
+import {Button, Dialog, FAB, Headline, Paragraph, Portal, Subheading, Text} from 'react-native-paper';
+
 const Tab = createMaterialTopTabNavigator();
 
 class PatientProfileScreen extends React.Component {
@@ -21,6 +18,8 @@ class PatientProfileScreen extends React.Component {
         this.state = {
             patient: {},
             loading: true,
+            newVisitDialogOpen: false,
+            visitState: null,
         }
     }
 
@@ -35,8 +34,8 @@ class PatientProfileScreen extends React.Component {
             .catch(err => {})
 
         doctorDao.getVisitState(this.props.route.params.userId)
-            .then(state => console.log('staet', state))
-            .catch(err => console.log('dao err', err));
+            .then(visitState => this.setState({visitState: visitState}))
+            .catch(err => {})
     }
 
     render() {
@@ -99,7 +98,12 @@ class PatientProfileScreen extends React.Component {
                 <FAB
                     style={styles.fab}
                     icon={'note-plus'}
-                    onPress={() => console.log('Pressed')}
+                    onPress={() => this.setState({newVisitDialogOpen: true})}
+                />
+                <StartVisitDialog
+                    visitState={this.state.visitState}
+                    visible={this.state.newVisitDialogOpen}
+                    onDismiss={() => this.setState({newVisitDialogOpen: false})}
                 />
             </ScreenLayout>
         );
@@ -111,8 +115,100 @@ export default PatientProfileScreen;
 const styles = StyleSheet.create({
     fab: {
         position: 'absolute',
-        margin: 16,
+        margin: 20,
         left: 0,
         bottom: 0,
     },
 })
+
+const StartVisitDialog = (props) => {
+    if (props.visitState == VisitState.FIRST_VISIT) {
+        return <StartFirstVisitDialog
+            visible={props.visible} onDismiss={props.onDismiss} dismissable={true}
+        />
+    }
+    else if (props.visitState == VisitState.FOLLOWUP_VISIT) {
+        return <FollowupVisitNotImplementedDialog
+            visible={props.visible} onDismiss={props.onDismiss} dismissable={true}
+        />
+    }
+    else if (props.visitState == VisitState.INCOMPLETE_VISIT) {
+        return <ContinueCachedVisitDialog
+            visible={props.visible} onDismiss={props.onDismiss} dismissable={true}
+        />
+    }
+    else return null;
+}
+
+// TODO Refactor: Reuse components
+const ContinueCachedVisitDialog = (props) => {
+    return (
+        <Portal>
+            <Dialog visible={props.visible} onDismiss={props.onDismiss} style={{paddingBottom: 10}} dismissable={false}>
+                <DialogMessage>ویزیت قبلی این بیمار ناتمام مانده. آیا میخواهید ویزیت قبلی را ادامه دهید؟</DialogMessage>
+                <Dialog.Actions style={visitDialogStyles.continueCached.dialogActions}>
+                    <Button style={{ }} mode="text" loading={props.loading} onPress={props.onContinue} >ادامه قبلی</Button>
+                    <Button style={{ }} mode="text" loading={props.loading} onPress={props.onBeginNew} >ویزیت جدید</Button>
+                    <Button disabled={!props.dismissable} style={{ }} mode="text" onPress={props.onDismiss} >انصراف</Button>
+                </Dialog.Actions>
+            </Dialog>
+        </Portal>
+    )
+}
+
+const StartFirstVisitDialog = (props) => {
+    return (
+        <Portal>
+            <Dialog visible={props.visible} onDismiss={props.onDismiss} style={{paddingBottom: 5}} dismissable={false}>
+                <DialogMessage>آیا میخواهید یک جلسه ویزیت را شروع کنید؟</DialogMessage>
+                <Dialog.Actions style={visitDialogStyles.firstVisit.dialogActions}>
+                    <Button style={{}} labelStyle={{padding: 5}} mode="text" loading={props.loading} onPress={props.onBeginNew} >بله</Button>
+                    <Button disabled={!props.dismissable} style={{}} labelStyle={{padding: 5}} mode="text" onPress={props.onDismiss} >خیر</Button>
+                </Dialog.Actions>
+            </Dialog>
+        </Portal>
+    )
+}
+
+const FollowupVisitNotImplementedDialog = (props) => {
+    return (
+        <Portal>
+            <Dialog visible={props.visible} onDismiss={props.onDismiss} style={{paddingBottom: 5}} dismissable={false}>
+                <DialogMessage>در حال حاضر فقط یکبار می‌توانید بیمار را ویزیت کنید.</DialogMessage>
+                <Dialog.Actions style={visitDialogStyles.notImplemented.dialogActions}>
+                    <Button disabled={false} style={{}} labelStyle={{padding: 5}} mode="text" onPress={props.onDismiss} >باشه</Button>
+                </Dialog.Actions>
+            </Dialog>
+        </Portal>
+    )
+}
+
+const DialogMessage = (props) => {return (
+    <Dialog.Content color={currentTheme.colors.placeholder} style={{paddingTop: 20}}>
+        <Subheading style={{textAlign: 'center'}}>{props.children}</Subheading>
+    </Dialog.Content>
+)}
+
+const visitDialogStyles = {
+    notImplemented: {
+        dialogActions: {
+            alignItems: 'center',
+            justifyContent: 'center',
+        }
+    },
+    firstVisit: {
+        dialogActions: {
+            alignItems: 'center',
+            justifyContent: 'space-around',
+        }
+    },
+    continueCached: {
+        dialogActions: {
+            alignItems: 'center',
+            justifyContent: 'space-around',
+        }
+    },
+    common: {
+
+    }
+}
---
+++ src/doctor/view/patients/visit/first/FirstVisitScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {StyleSheet, View} from "react-native";
+import {Button, Text} from "react-native-paper";
+
+class FirstVisitScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render() {
+        return (
+            <View></View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
